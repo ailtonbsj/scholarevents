e2f643991a3e97226e23da90236fcc2d8eddb826
ailtonbsj <ailton.ifce@gmail.com>
2019-09-12 16:17:11 -0300

    fixed themes to php7

--- admin/temaajax.php
+++ admin/temaajax.php
@@ -5,26 +5,26 @@ require '../link1.php';
 $dados = $_POST['id'];
 
 foreach ($dados as $reg) {
-    $$reg['name'] = $reg['value'];
+    ${$reg['name']} = $reg['value'];
 }
 
 $sqlres = "UPDATE sch_temas SET ativo = 'F'";
-$busca3 = mysql_query($sqlres);
+$busca3 = $link1->query($sqlres);
 if(!$busca3){
     echo 'ERROR';
     exit;
 }
 
 $sqlbusca = "SELECT * FROM sch_temas WHERE (id='$idtheme') AND (block = 'T')";
-$busca = mysql_query($sqlbusca);
+$busca = $link1->query($sqlbusca);
 if(!$busca){
     echo 'ERROR';
     exit;
 }
 
-if(mysql_num_rows($busca) == 1){
+if($busca->rowCount() == 1){
     $sqlres = "UPDATE sch_temas SET ativo = 'T' WHERE id = '$idtheme'";
-    $busca = mysql_query($sqlres);
+    $busca = $link1->query($sqlres);
     if(!$busca){
         echo 'ERROR';
         exit;
@@ -33,7 +33,7 @@ if(mysql_num_rows($busca) == 1){
 }
 else {
     $sqlmod = "UPDATE sch_temas SET cor1 = '$cor1', cor2 = '$cor2', cor3 = '$cor3', cor4 = '$cor4', cor5 = '$cor5', cor6 = '$cor6', block = 'F', ativo = 'T' WHERE id = '$idtheme'";
-    $busca = mysql_query($sqlmod);
+    $busca = $link1->query($sqlmod);
     if(!$busca){
         echo 'ERROR';
         exit;
